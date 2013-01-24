7e0acdc26ef5aa19b6ea677ee65199a05c02400b
Han <szu.han.chang@gmail.com>
2013-01-24 18:38:17 +0800

    Add title, fix typo

--- app.rb
+++ app.rb
@@ -2,15 +2,18 @@ require 'sinatra'
 
 get '/' do
   @resume = "active"
+  @title = 'Szu Han Chang - Resume'
   haml :resume
 end
 
 get '/projects' do
   @projects = "active"
+  @title = 'Szu Han Chang - Projects'
   haml :projects
 end
 
 get '/coursework' do
   @coursework = "active"
+  @title = 'Szu Han Chang - Academic Coursework'
   haml :coursework
 end
