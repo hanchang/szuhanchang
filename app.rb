5959dbe664bccfb4e094ebc03f0ec64cd14559cb
Han <szu.han.chang@gmail.com>
2013-01-25 12:06:42 +0800

    Add canonical urls

--- app.rb
+++ app.rb
@@ -3,17 +3,20 @@ require 'sinatra'
 get '/' do
   @resume = "active"
   @title = 'Szu Han Chang - Resume'
+  @canonical = 'http://www.szuhanchang.com/'
   haml :resume
 end
 
 get '/projects' do
   @projects = "active"
   @title = 'Szu Han Chang - Projects'
+  @canonical = 'http://www.szuhanchang.com/projects.html'
   haml :projects
 end
 
 get '/coursework' do
   @coursework = "active"
   @title = 'Szu Han Chang - Academic Coursework'
+  @canonical = 'http://www.szuhanchang.com/coursework.html'
   haml :coursework
 end
