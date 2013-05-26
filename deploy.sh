bfd081bb95ef1cfc779a906af801c824a7457adf
Han Chang <szu.han.chang@gmail.com>
2013-05-26 22:03:33 +0000

    Fix minor bugs

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 rm -rf site
 wget --convert-links --recursive --level=inf --no-host-directories --no-remove-listing --html-extension --directory-prefix=site http://localhost:4567
 # This replaces the .html extension with just the directory since on the shared host there is no HTML file.
-sed -i 's|bucs/cs\([0-9]\+\).1.html|/bucs/cs\1|g' site/coursework.html
+sed -i 's|bucs/cs\([0-9]\+\).html|/bucs/cs\1|g' site/coursework.html
+
+# The BUCS content is located directly on the shared host and is not part of the szuhanchang repository.
+rm -rf site/bucs
