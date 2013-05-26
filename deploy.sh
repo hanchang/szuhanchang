be52fbe1228442c9dd8b7b7037619fe8a69e7a10
Han Chang <szu.han.chang@gmail.com>
2013-05-26 21:55:17 +0000

    Clean up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+rm -rf site
 wget --convert-links --recursive --level=inf --no-host-directories --no-remove-listing --html-extension --directory-prefix=site http://localhost:4567
-#sed -i 's|http://szuhanchang.herokuapp.com/bucs/cs\([0-9]\+\)|/bucs/cs\1|g' coursework.html
-sed -i 's|bucs/cs\([0-9]\+\).1.html|/bucs/cs\1|g' coursework.html
+# This replaces the .html extension with just the directory since on the shared host there is no HTML file.
+sed -i 's|bucs/cs\([0-9]\+\).1.html|/bucs/cs\1|g' site/coursework.html
